8270193128c7a4303e47e32c63ec3af5115f433f
shahaklt <ltshahak@gmail.com>
2026-05-03 10:50:44 -0400

    Patch Pearson correlation network construction

--- pipeline/enrichment.py
+++ pipeline/enrichment.py
@@ -23,3 +23,5 @@ def run_go_enrichment(module_csv: Path, outdir: Path, org_db: str = "org.Hs.eg.d
 # wip note: refactor multi-group contrast caveat note
 
 # wip note: clarify module_hubs.csv writer
+
+# wip note: patch pearson correlation network construction
