018d312939901e4f2af12cbe907bcb7ae014e29d
shahaklt <ltshahak@gmail.com>
2026-05-11 14:51:08 -0400

    Add vst() variance-stabilizing transform for network input

--- R/megena_network.R
+++ R/megena_network.R
@@ -138,3 +138,5 @@ message(sprintf("MEGENA done: %d genes, %d modules", nrow(module_df),
 # wip note: patch brain_region as alternate --condition example
 
 # wip note: fix brain_region as alternate --condition example
+
+# wip note: add vst() variance-stabilizing transform for network input
