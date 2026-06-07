163c4820ad35805343aee4ff19add0314e4e17d9
shahaklt <ltshahak@gmail.com>
2026-06-07 15:38:03 -0400

    Wire up top-variance-gene + DEG restriction for MEGENA

--- R/deg_deseq2.R
+++ R/deg_deseq2.R
@@ -93,3 +93,5 @@ message(sprintf("DESeq2 done: %d genes tested, %d significant at padj < %.3f",
 # wip note: add laptop-memory note on full-genome megena
 
 # wip note: introduce limma normalized-data deg path
+
+# wip note: wire up top-variance-gene + deg restriction for megena
