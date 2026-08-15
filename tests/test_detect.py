fc7292270e8fccca90f7b08a57b1cd2d6a4b1619
shahaklt <ltshahak@gmail.com>
2026-08-15 12:19:51 -0400

    Correct CLI deg subcommand

--- tests/test_detect.py
+++ tests/test_detect.py
@@ -71,3 +71,5 @@ def test_validate_metadata_requires_two_groups():
 # wip note: correct design-formula construction from metadata
 
 # wip note: improve module_hubs.csv writer
+
+# wip note: correct cli deg subcommand
